4b53a7286b572e7d1cb5253fd05a197d408bb0f6
wwqgtxx <wwqgtxx@gmail.com>
2018-02-25 14:27:28 +0800

    adapt wwqLyParse
    Signed-off-by: wwqgtxx <wwqgtxx@gmail.com>

---
+++ lulu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+_srcdir = '%s/' % os.path.dirname(os.path.realpath(__file__))
+_filepath = os.path.dirname(sys.argv[0])
+sys.path.insert(1, os.path.join(_filepath, _srcdir))
+sys.path.insert(1, os.path.join(_filepath, '../lib/lulu_lib/'))
+
+
+if sys.version_info[0] == 3:
+    import lulu
+    if __name__ == '__main__':
+        lulu.main()
+else:  # Python 2
+    from lulu.util import log
+    log.e('[fatal] Python 3 is required!')
